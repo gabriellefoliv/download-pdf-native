85a6d3506340070a2e2383b3311449f048703be4
Gabrielle Oliveira <oliveiragabrielle662@gmail.com>
2024-03-17 00:08:53 -0300

    feat: open + save pdf

--- src/screens/Download/index.tsx
+++ src/screens/Download/index.tsx
@@ -1,8 +1,10 @@
-import { Alert, Text, View } from "react-native";
+import { Alert, Platform, Text, View } from "react-native";
 import { styles } from "./styles";
 import { Button } from "../../components/Button";
 
 import * as FileSystem from 'expo-file-system'
+import * as Sharing from 'expo-sharing'
+
 import { useState } from "react";
 
 const PDF_URI = {
@@ -40,12 +42,33 @@ export function Download() {
             const downloadResponse = await downloadResumable.downloadAsync()
 
             if(downloadResponse?.uri) {
+                await fileSave(downloadResponse.uri, PDF_NAME)
                 setIsDownloading(false)
+                setPercentageProgress(0)
             }
         } catch (error) {
             Alert.alert("Download", "Não foi possível fazer o download.")
             console.log(error)
         } 
+    }
+
+    async function fileSave(uri: string, filename: string) {
+        if(Platform.OS == 'android') {
+            const directoryUri = FileSystem.cacheDirectory + filename
+
+            const base64File = await FileSystem.readAsStringAsync(uri, {
+                encoding: FileSystem.EncodingType.Base64
+            })
+
+            await FileSystem.writeAsStringAsync(directoryUri, base64File, {
+                encoding: FileSystem.EncodingType.Base64
+            })
+
+            await Sharing.shareAsync(directoryUri)
+
+        } else {
+            Sharing.shareAsync(uri)
+        }
     }
 
     return (
